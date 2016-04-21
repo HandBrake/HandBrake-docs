c3ca16eda070c44e941f0f8a6bd57f696e802b99
Bradley Sepos <bradley@bradleysepos.com>
2016-04-21 12:09:50 -0400

    template: Remember system selection via cookie.

--- templates/mark1/js/scripts.js
+++ templates/mark1/js/scripts.js
@@ -11,19 +11,10 @@ $(document).ready(function() {
     var system_mac = $('.system-mac');
     var system_win = $('.system-win');
 
-    var system_switch = function(control){
+    var system_switch = function(control, scroll){
         var classes = $(control).attr('class').split(' ');
         classes.forEach(function(target){
             switch (target){
-                case 'system-select-all':
-                    system_select_all.addClass('selected');
-                    system_select_lin.removeClass('selected');
-                    system_select_mac.removeClass('selected');
-                    system_select_win.removeClass('selected');
-                    system_lin.removeClass('hidden');
-                    system_mac.removeClass('hidden');
-                    system_win.removeClass('hidden');
-                    break;
                 case 'system-select-lin':
                     system_select_lin.addClass('selected');
                     system_select_all.removeClass('selected');
@@ -32,6 +23,7 @@ $(document).ready(function() {
                     system_mac.addClass('hidden');
                     system_win.addClass('hidden');
                     system_lin.removeClass('hidden');
+                    Cookies.set('system', 'lin', { expires: 7 });
                     break;
                 case 'system-select-mac':
                     system_select_mac.addClass('selected');
@@ -41,6 +33,7 @@ $(document).ready(function() {
                     system_lin.addClass('hidden');
                     system_win.addClass('hidden');
                     system_mac.removeClass('hidden');
+                    Cookies.set('system', 'mac', { expires: 7 });
                     break;
                 case 'system-select-win':
                     system_select_win.addClass('selected');
@@ -50,18 +43,47 @@ $(document).ready(function() {
                     system_lin.addClass('hidden');
                     system_mac.addClass('hidden');
                     system_win.removeClass('hidden');
+                    Cookies.set('system', 'win', { expires: 7 });
                     break;
                 default:
+                    system_select_all.addClass('selected');
+                    system_select_lin.removeClass('selected');
+                    system_select_mac.removeClass('selected');
+                    system_select_win.removeClass('selected');
+                    system_lin.removeClass('hidden');
+                    system_mac.removeClass('hidden');
+                    system_win.removeClass('hidden');
+                    Cookies.remove('system');
                     break;
             }
         });
-        $("html, body").animate({ scrollTop: 0 }, 250);
+        if (scroll === true){
+            $("html, body").animate({ scrollTop: 0 }, 250);
+        }
     };
 
     system_select.find('li').click(function(){
-        system_switch(this);
+        system_switch(this, true);
     });
-    system_select_all.addClass('selected');
+    var system = Cookies.get('system');
+    if (typeof system != 'undefined'){
+        switch (system){
+            case 'lin':
+                system_switch(system_select_lin, false);
+                break;
+            case 'mac':
+                system_switch(system_select_mac, false);
+                break;
+            case 'win':
+                system_switch(system_select_win, false);
+                break;
+            default:
+                system_switch(system_select_all, false);
+                break;
+        }
+    } else {
+        system_switch(system_select_all, false);
+    }
 
     $('article.docs figure > a:has(img)').featherlight({type: 'image', openSpeed: 150, closeSpeed: 150});
 });
