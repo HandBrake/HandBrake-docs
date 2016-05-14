867ab52e350f1cfe1f52a2f3ede298516b8200a8
Bradley Sepos <bradley@bradleysepos.com>
2016-05-14 08:20:28 -0400

    template: Clean up some JavaScript.

--- templates/mark1/js/scripts.js
+++ templates/mark1/js/scripts.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
     $('body').addClass('js');
 
     if (!$('body').hasClass("license")){
-        var navigation = $('article.docs nav .navigation');
-        var navigation_headings = navigation.find('h2');
+        var navigation_headings = $('article.docs nav .navigation > h2');
+        var navigation_sections = navigation_headings.next('ul');
         navigation_headings.click(function(){
             $(this).addClass('selected').next('ul').slideDown(250);
             navigation_headings.not(this).removeClass('selected').next('ul').slideUp(250);
         });
-        navigation_headings.next('ul').has('a.selected').show().prev('h2').addClass('selected');
-        navigation_headings.next('ul').not(':has(a.selected)').hide();
+        navigation_sections.has('a.selected').show().prev('h2').addClass('selected');
+        navigation_sections.not(':has(a.selected)').hide();
 
         var nav = $('article.docs nav');
         var system_select = $('<ul class="system-select">Instructions:<li class="system-select-all">All</li><li class="system-select-lin">Linux</li><li class="system-select-mac">Mac</li><li class="system-select-win">Windows</li></ul>').prependTo(nav);
