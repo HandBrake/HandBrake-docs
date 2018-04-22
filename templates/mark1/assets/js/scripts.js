f9bff56f47ec48f6d7c889a4b841ada93e0c063b
Bradley Sepos <bradley@bradleysepos.com>
2018-04-22 11:57:44 -0400

    templates: Add support for #system-foo linking.
    Some under the hood verbosity to make things easier.

--- templates/mark1/assets/js/scripts.js
+++ templates/mark1/assets/js/scripts.js
@@ -20,10 +20,11 @@ $(document).ready(function(){
 
         var nav = docs.find('nav');
         var system_select = docs.find('.system-select');
-        var system_select_lin = system_select.find('.system-select-lin');
-        var system_select_mac = system_select.find('.system-select-mac');
-        var system_select_win = system_select.find('.system-select-win');
+        var system_select_linux = system_select.find('.system-select-linux');
+        var system_select_macos = system_select.find('.system-select-macos');
+        var system_select_windows = system_select.find('.system-select-windows');
         var system_select_all = system_select.find('.system-select-all');
+        var system = '';
 
         var scroll_to_section = function(el, fallback_el){
             var headings = 'h1, h2, h3, h4, h5, h6';
@@ -51,109 +52,111 @@ $(document).ready(function(){
         };
 
         var system_switch = function(control, scroll){
-            var system_all = docs.find('.system-lin, .system-mac, .system-win');
-            var system_lin = system_all.filter('.system-lin');
-            var system_mac = system_all.filter('.system-mac');
-            var system_win = system_all.filter('.system-win');
+            var system_all_sections = docs.find('.system-linux, .system-macos, .system-windows');
+            var system_linux_sections = system_all_sections.filter('.system-linux');
+            var system_macos_sections = system_all_sections.filter('.system-macos');
+            var system_windows_sections = system_all_sections.filter('.system-windows');
             var classes = $(control).attr('class').split(' ');
             classes.forEach(function(target){
                 switch (target){
-                    case 'system-select-lin':
-                        if (system_select_lin.hasClass('selected')){
+                    case 'system-select-linux':
+                        if (system_select_linux.hasClass('selected')){
                             break;
                         }
-                        system_select_lin.addClass('selected');
+                        system_select_linux.addClass('selected');
                         system_select_all.removeClass('selected');
-                        system_select_mac.removeClass('selected');
-                        system_select_win.removeClass('selected');
-                        system_mac.addClass('hidden');
-                        system_win.addClass('hidden');
-                        system_lin.removeClass('hidden');
+                        system_select_macos.removeClass('selected');
+                        system_select_windows.removeClass('selected');
+                        system_macos_sections.addClass('hidden');
+                        system_windows_sections.addClass('hidden');
+                        system_linux_sections.removeClass('hidden');
                         if (scroll === true){
-                            scroll_to_section(system_lin.first(), system_all.first());
+                            scroll_to_section(system_linux_sections.first(), system_all_sections.first());
                         }
-                        Cookies.set('system', 'lin', { expires: 7 });
+                        Cookies.set('hbdocs-system', 'system-linux', { expires: 7 });
                         break;
-                    case 'system-select-mac':
-                        if (system_select_mac.hasClass('selected')){
+                    case 'system-select-macos':
+                        if (system_select_macos.hasClass('selected')){
                             break;
                         }
-                        system_select_mac.addClass('selected');
+                        system_select_macos.addClass('selected');
                         system_select_all.removeClass('selected');
-                        system_select_lin.removeClass('selected');
-                        system_select_win.removeClass('selected');
-                        system_lin.addClass('hidden');
-                        system_win.addClass('hidden');
-                        system_mac.removeClass('hidden');
+                        system_select_linux.removeClass('selected');
+                        system_select_windows.removeClass('selected');
+                        system_linux_sections.addClass('hidden');
+                        system_windows_sections.addClass('hidden');
+                        system_macos_sections.removeClass('hidden');
                         if (scroll === true){
-                            scroll_to_section(system_mac.first(), system_all.first());
+                            scroll_to_section(system_macos_sections.first(), system_all_sections.first());
                         }
-                        Cookies.set('system', 'mac', { expires: 7 });
+                        Cookies.set('hbdocs-system', 'system-macos', { expires: 7 });
                         break;
-                    case 'system-select-win':
-                        if (system_select_win.hasClass('selected')){
+                    case 'system-select-windows':
+                        if (system_select_windows.hasClass('selected')){
                             break;
                         }
-                        system_select_win.addClass('selected');
+                        system_select_windows.addClass('selected');
                         system_select_all.removeClass('selected');
-                        system_select_lin.removeClass('selected');
-                        system_select_mac.removeClass('selected');
-                        system_lin.addClass('hidden');
-                        system_mac.addClass('hidden');
-                        system_win.removeClass('hidden');
+                        system_select_linux.removeClass('selected');
+                        system_select_macos.removeClass('selected');
+                        system_linux_sections.addClass('hidden');
+                        system_macos_sections.addClass('hidden');
+                        system_windows_sections.removeClass('hidden');
                         if (scroll === true){
-                            scroll_to_section(system_win.first(), system_all.first());
+                            scroll_to_section(system_windows_sections.first(), system_all_sections.first());
                         }
-                        Cookies.set('system', 'win', { expires: 7 });
+                        Cookies.set('hbdocs-system', 'system-windows', { expires: 7 });
                         break;
                     case 'system-select-all':
                         if (system_select_all.hasClass('selected')){
                             break;
                         }
                         system_select_all.addClass('selected');
-                        system_select_lin.removeClass('selected');
-                        system_select_mac.removeClass('selected');
-                        system_select_win.removeClass('selected');
-                        system_lin.removeClass('hidden');
-                        system_mac.removeClass('hidden');
-                        system_win.removeClass('hidden');
+                        system_select_linux.removeClass('selected');
+                        system_select_macos.removeClass('selected');
+                        system_select_windows.removeClass('selected');
+                        system_linux_sections.removeClass('hidden');
+                        system_macos_sections.removeClass('hidden');
+                        system_windows_sections.removeClass('hidden');
                         if (scroll === true){
-                            scroll_to_section(system_all.first());
+                            scroll_to_section(system_all_sections.first());
                         }
-                        Cookies.remove('system');
+                        Cookies.remove('hbdocs-system');
                         break;
                     default:
                         break;
                 }
-                var location_sans_fragment = window.location.href.split('#')[0];
-                if (window.location != location_sans_fragment){
-                    history.pushState(null, null, location_sans_fragment);
-                }
             });
         };
+
+        var system_set = function(){
+            if ($.inArray(window.location.hash, ['#system-linux', '#system-macos', '#system-windows']) !== -1){
+                system = window.location.hash.substring(1);
+            }
+            switch (system){
+                case 'system-linux':
+                    system_switch(system_select_linux, false);
+                    break;
+                case 'system-macos':
+                    system_switch(system_select_macos, false);
+                    break;
+                case 'system-windows':
+                    system_switch(system_select_windows, false);
+                    break;
+                default:
+                    system = '';
+                    system_switch(system_select_all, false);
+                    break;
+            }
+            return false;
+        }
 
         system_select.find('li').click(function(){
             system_switch(this, true);
         });
-        var system = Cookies.get('system');
-        if (typeof system != 'undefined'){
-            switch (system){
-                case 'lin':
-                    system_switch(system_select_lin, false);
-                    break;
-                case 'mac':
-                    system_switch(system_select_mac, false);
-                    break;
-                case 'win':
-                    system_switch(system_select_win, false);
-                    break;
-                default:
-                    system_switch(system_select_all, false);
-                    break;
-            }
-        } else {
-            system_switch(system_select_all, false);
-        }
+        system = Cookies.get('hbdocs-system') || '';
+        $(window).bind('hashchange', system_set);
+        system_set();
     }
 
     // temporary hack until build system creates breadcrumbs instead of template
